4d6c0b6cf3675674a19486ed5ede722614c9fa89
BlackRobinhood <tpo16@hotmail.com>
2013-06-05 23:37:59 +0200

    changes

--- Home.php
+++ Home.php
@@ -92,7 +92,7 @@
 			  Learn more about options for the login button plugin:
 			  /docs/reference/plugins/login/ -->
 
-			<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
+			<button class="loginButton">Login</button>
 		
 
 </body>
