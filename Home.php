b264063a790ba6fa739d8b8704726beb85fdfb7f
BlackRobinhood <tpo16@hotmail.com>
2013-06-05 23:30:34 +0200

    changes

--- Home.php
+++ Home.php
@@ -60,6 +60,20 @@
 				console.log(response);
  
 				}); //FB.login
+				
+				 FB.api('/me', function(response) {
+				// stuff you want to happen after getting data goes here
+				alert(response.name);
+				console.log(response);
+ 
+				}); 
+				
+				 FB.api('/me/friends', function(response) {
+				//stuff you want to happen after getting data goes here
+				console.log(response);
+				alert(response.data[0].name);
+ 
+				}); //FB.api
 
 			  // Here we run a very simple test of the Graph API after login is successful. 
 			  // This testAPI() function is only called in those cases. 
