5224db0aa75a8c24ac3234d2b8676754ba0e17c5
BlackRobinhood <tpo16@hotmail.com>
2013-06-06 00:42:38 +0200

    changes

--- js/hello.js
+++ js/hello.js
@@ -4,7 +4,22 @@ $('.loginButton').click(function()
 	{
 		// stuff you want to happen after login goes here
 		console.log(response);
+		
  
-	},{scope: 'publish_actions'}); //FB.login
+	}); //FB.login
+	FB.api('/me', function(response) {
+				// stuff you want to happen after getting data goes here
+				alert(response.name);
+				console.log(response);
+ 
+				}); 
+				
+				 FB.api('/me/friends', function(response) {
+				//stuff you want to happen after getting data goes here
+				console.log(response);
+				alert(response.data[0].name);
+ 
+				}); //FB.api
+
 				
 });
